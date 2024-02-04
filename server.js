55f0bdf94ec28c4178078b5eeb8c3d2761fb066b
Shuttleu <shuttleu@gmail.com>
2024-02-04 21:34:21 +0000

    Add some stats

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
 
 const db = require("./models");
 var cors = require('cors')
@@ -15,8 +15,8 @@ app.get('/users', async (req, res) => {
 })
 
 app.get('/barks', async (req, res) => {
-  const { count, bloops } = await db.Bloop.findAndCountAll();
-  res.json(count);
+  const bloopCount = await db.Bloop.count();
+  res.json(bloopCount);
 })
 
 app.get('/users/achievements', async (req, res) => {
@@ -27,6 +27,26 @@ app.get('/users/achievements', async (req, res) => {
 app.get('/achievements', async (req, res) => {
   const achievements = await db.Achievement.findAll();
   res.json(achievements);
+})
+
+app.get('/stats', async (req, res) => {
+    let boxNumbers = [];
+    const boxes = await db.Box.findAll();
+    const boxCount = await db.Box.count();
+    const bloopCount = await db.Bloop.count();
+    const userCount = await db.User.count();
+    for (let box in boxes) {
+        const boxBloopCount = await boxes[box].countBloops();
+        boxNumbers.push({ name: boxes[box].name, count: boxBloopCount });
+
+    }
+    res.json({
+        bloopsPerBox: boxNumbers,
+        numberOfBloops: bloopCount,
+        meanPerBox: Math.floor(bloopCount/boxCount),
+        numberOfUsers: userCount,
+        meanPerUser: Math.floor(bloopCount/userCount)
+    });
 })
 
 app.get('/user/:id', async (req, res) => {
