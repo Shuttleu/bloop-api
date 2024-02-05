4df05b1ccd486545a1795dafd4db5d16cdb34b48
Shuttleu <shuttleu@gmail.com>
2024-02-05 15:33:18 +0000

    More stats

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
 
 const db = require("./models");
 var cors = require('cors')
@@ -30,24 +30,52 @@ app.get('/achievements', async (req, res) => {
 })
 
 app.get('/stats', async (req, res) => {
-    let boxNumbers = [];
-    const boxes = await db.Box.findAll();
-    const boxCount = await db.Box.count();
-    const bloopCount = await db.Bloop.count();
-    const userCount = await db.User.count();
-    for (let box in boxes) {
-        const boxBloopCount = await boxes[box].countBloops();
-        boxNumbers.push({ name: boxes[box].name, count: boxBloopCount });
+  let boxNumbers = [];
+  let days = [];
+  for (let day = 1; day <= 5; day++) {
+    days[day - 1] = { dayCount: 0, hourCount: [] };
+    let dayCount = 0;
+    for (let hour = 0; hour <= 23; hour++) {
+      const hourCount = await db.Bloop.count({ where: { createdAt: { [Op.between]: [`2024-02-${day} ${hour}:00:00.0+00`, `2024-02-${day} ${hour}:59:59.999+00`], } } });
+      dayCount += hourCount;
+      days[day - 1].hourCount.push(hourCount);
+    }
+    days[day - 1].dayCount = dayCount;
+  }
+  const boxes = await db.Box.findAll();
+  const boxCount = await db.Box.count();
+  const bloopCount = await db.Bloop.count();
+  const userCount = await db.User.count();
+  let daysBoxes = {};
+  for (let box in boxes) {
+    for (let day = 1; day <= 5; day++) {
+      daysBoxes[boxes[box].name] = []
+      for (let day = 1; day <= 5; day++) {
+        daysBoxes[boxes[box].name][day - 1] = { dayCount: 0, hourCount: [] };
+        let dayCount = 0;
+        for (let hour = 0; hour <= 23; hour++) {
+          const hourCount = await db.Bloop.count({ where: { createdAt: { [Op.between]: [`2024-02-${day} ${hour}:00:00.0+00`, `2024-02-${day} ${hour}:59:59.999+00`], } } });
+          dayCount += hourCount;
+          daysBoxes[boxes[box].name][day - 1].hourCount.push(hourCount);
+        }
+        daysBoxes[boxes[box].name][day - 1].dayCount = dayCount;
+      }
 
     }
-    res.json({
-        bloopsPerBox: boxNumbers,
-        numberOfBloops: bloopCount,
-        meanPerBox: Math.floor(bloopCount/boxCount),
-        numberOfUsers: userCount,
-        meanPerUser: Math.floor(bloopCount/userCount)
-    });
-})
+    const boxBloopCount = await boxes[box].countBloops();
+    boxNumbers.push({ name: boxes[box].name, count: boxBloopCount });
+  }
+  res.json({
+    bloopsPerBox: boxNumbers,
+    numberOfBloops: bloopCount,
+    meanPerBox: Math.floor(bloopCount / boxCount),
+    numberOfUsers: userCount,
+    meanPerUser: Math.floor(bloopCount / userCount),
+    bloopsPerDayHour: days,
+    BloopsPerBoxDayHour: daysBoxes
+  });
+}
+);
 
 app.get('/user/:id', async (req, res) => {
   const user = await db.User.findByPk(req.params.id, { include: [db.Achievement] });
